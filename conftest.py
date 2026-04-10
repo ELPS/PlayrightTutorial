cfc72c376f6cd339b3b1f0173161621af661374d
ELPS <leandro_pereirasantos@msn.com>
2026-04-10 10:58:42 +0100

    corrections

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from playwright.sync_api import Playwright
+import time
+import utils.secret_config
 
 
-@pytest.fixture
-def set_up(playwright: Playwright):
+
+
+@pytest.fixture(scope="session")
+def set_up(browser):
     # Access - Given
-    browser = playwright.chromium.launch(headless=False)
+    #browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     # Open new page
     page = context.new_page()
@@ -13,3 +17,43 @@ def set_up(playwright: Playwright):
     page.set_default_timeout(3000)
 
     yield page
+    page.close()
+
+
+@pytest.fixture(scope="session")
+def login_set_up(set_up):
+
+    page = set_up
+
+    login_issue = True
+    while login_issue:
+        if not page.is_visible("[data-testid=\"signUp.switchToSignUp\"]"):
+            page.click("button:has-text(\"Log In\")")
+        else:
+            login_issue = False
+        time.sleep(0.1)
+
+
+    page.click("[data-testid=\"signUp.switchToSignUp\"]", timeout=2000)
+
+    # page.click("'Log In'", timeout=2000)
+    # page.click("[data-testid=\"signUp.switchToSignUp\"]")
+    page.click("[data-testid='switchToEmailLink'] >> [data-testid='buttonElement']")
+    # page.click("[data-testid='siteMembersContainer'] input[type='email']")
+    # page.click("[data-testid='siteMembersContainer'] input[type='email']", "symon.storozhenko@gmail.com")
+    page.fill('input:below(:text("Email"))', "symon.storozhenko@gmail.com")
+    page.press("[data-testid='siteMembers.container'] >> input[type='email']", "Tab")
+    page.fill("input[type='password']", utils.secret_config.PASSWORD)
+    page.click("[data-testid='submit'] >> [data-testid='buttonElement']")
+    # page.click("[aria-label='symon.storozhenko account menu']")
+
+    yield page
+
+
+@pytest.fixture
+def go_to_new_collection_page(page):
+    page.goto("/new-collection")
+    page.set_default_timeout(3000)
+
+    yield page
+
--- tests_ui_layout/test_login.py
+++ tests_ui_layout/test_login.py
@@ -4,6 +4,8 @@ from idlelib import browser
 from playwright.sync_api import Playwright, sync_playwright
 import pytest
 
+import utils.secret_config
+
 
 def test_login(set_up) -> None:
     # Access - Given
@@ -31,9 +33,10 @@ def test_login(set_up) -> None:
     # page.click("[data-testid='siteMembersContainer'] input[type='email']", "symon.storozhenko@gmail.com")
     page.fill('input:below(:text("Email"))', "symon.storozhenko@gmail.com")
     page.press("[data-testid='siteMembers.container'] >> input[type='email']", "Tab")
-    page.fill("input[type='password']", "test123")
+    page.fill("input[type='password']", utils.secret_config.PASSWORD)
     page.click("[data-testid='submit'] >> [data-testid='buttonElement']")
-    page.click("[aria-label='symon.storozhenko account menu']")
+    #page.click("[aria-label='symon.storozhenko account menu']")
+
 
     # Assert - Then
     assert page.is_visible("text=My Orders")
