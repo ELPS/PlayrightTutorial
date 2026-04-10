e222637481351298e1274da35eac09c4157a14bb
ELPS <leandro_pereirasantos@msn.com>
2026-04-10 11:06:18 +0100

    added PASSWORD secret env to yml

--- conftest.py
+++ conftest.py
@@ -43,7 +43,8 @@ def login_set_up(set_up):
     # page.click("[data-testid='siteMembersContainer'] input[type='email']", "symon.storozhenko@gmail.com")
     page.fill('input:below(:text("Email"))', "symon.storozhenko@gmail.com")
     page.press("[data-testid='siteMembers.container'] >> input[type='email']", "Tab")
-    page.fill("input[type='password']", utils.secret_config.PASSWORD)
+    #page.fill("input[type='password']", utils.secret_config.PASSWORD)
+    page.fill("input[type='password']", os.environ['PASSWORD'])
     page.click("[data-testid='submit'] >> [data-testid='buttonElement']")
     # page.click("[aria-label='symon.storozhenko account menu']")
 
